80bffb90e38af235d5ef69e986b4cf05e9fcf907
jinshendan <jinshendan@gmail.com>
2020-04-20 10:34:17 +0200

    add Product-of-Array-Except-Self

---
+++ 30-Day-LeetCoding-Challenge/Week-3/Product-of-Array-Except-Self/cpp/Product-of-Array-Except-Self.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n, 1);
+        for (int i = 1; i < n; i++)
+            output[i] = output[i-1] * nums[i-1];
+        int prod = 1;
+        for (int i = n-2; i>=0; i--){
+            prod *= nums[i+1];
+            output[i] *= prod; 
+        }
+        return output;
+    }
+};
